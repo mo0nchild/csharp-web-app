ecffc04afd3b7a114173d912f2b5050328c059b7
mo0nchild <letsplaytoday778@gmail.com>
2022-01-26 18:44:27 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web_app.Models;
+
+namespace Web_app.Controllers;
+
+public class Shit : Controller
+{
+    public IActionResult Index()
+    { 
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        ViewBag.C = "dasd";
+        return View("Privacy", new ListModel { Id = 123, Name = "Mike" });
+
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ListModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_app.Models
+{
+    public class ListModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Null";
+    }
+}
